395c3ba8ceee916a2d687f91842c0baff8f58441
DuyManh1905 <93384645+DuyManh1905@users.noreply.github.com>
2024-09-12 12:18:21 +0700

    complete waveformview and button cancel

--- app/src/main/java/com/duymanh/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/duymanh/audiorecorder/MainActivity.kt
@@ -6,19 +6,31 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
 import com.duymanh.audiorecorder.databinding.ActivityMainBinding
+import com.duymanh.audiorecorder.databinding.BottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import java.io.File
 
 const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
     private lateinit var binding: ActivityMainBinding // View Binding object
+    private lateinit var amplitudes: ArrayList<Float>
 
     private var permissions = arrayOf(
         Manifest.permission.RECORD_AUDIO,
@@ -36,11 +48,14 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
     private lateinit var vibrator: Vibrator
     private lateinit var timer: Timer
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Initialize the View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         timer = Timer(this)
@@ -54,6 +69,11 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
         }
 
+
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheet))
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
         // Set up button click listener
         binding.btnRecord.setOnClickListener {
             when {
@@ -64,7 +84,66 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
             vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
         }
+        binding.btnList.setOnClickListener{
+            // TODO
+            Toast.makeText(this,"List button",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnDone.setOnClickListener{
+            stopRecorder()
+            Toast.makeText(this,"Record saved",Toast.LENGTH_SHORT).show()
+
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            binding.bottomSheetBG.visibility = View.VISIBLE
+            findViewById<TextInputEditText>(R.id.fileNameInput).setText(fileName)
+        }
+
+
+
+        findViewById<MaterialButton>(R.id.btnCancel).setOnClickListener{
+            File("$dirPath$fileName.mp3").delete()
+            dismiss()
+        }
+
+        findViewById<MaterialButton>(R.id.btnOk).setOnClickListener{
+            dismiss()
+            save()
+        }
+
+        binding.bottomSheetBG.setOnClickListener{
+            File("$dirPath$fileName.mp3").delete()
+            dismiss()
+        }
+
+        binding.btnDelete.setOnClickListener{
+            stopRecorder()
+            File("$dirPath$fileName.mp3").delete()
+            Toast.makeText(this,"Record delete",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnDelete.isClickable = false;
     }
+
+    private fun save(){
+        val newFilename = findViewById<TextInputEditText>(R.id.fileNameInput).text.toString()
+        if(newFilename!=fileName){
+            var newFile = File("$dirPath$newFilename.mp3")
+            File("$dirPath$fileName.mp3").renameTo(newFile)
+        }
+    }
+    private fun dismiss(){
+        binding.bottomSheetBG.visibility = View.GONE
+        hideKeyboard(findViewById<TextInputEditText>(R.id.fileNameInput))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        },100)
+    }
+    private fun hideKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -125,13 +204,37 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
         isPaused = false
 
         timer.start()
+        binding.btnDelete.isClickable = true
+        binding.btnDelete.setImageResource(R.drawable.ic_delete) // Use binding
+
+        binding.btnList.visibility = View.GONE
+        binding.btnDone.visibility = View.VISIBLE
+
     }
 
     private fun stopRecorder() {
         timer.stop()
+
+        recorder.apply {
+            stop()
+            release()
+        }
+
+        isPaused = false
+        isRecording = false
+
+        binding.btnList.visibility = View.VISIBLE
+        binding.btnDone.visibility = View.GONE
+
+        binding.btnDelete.isClickable = false
+        binding.btnDelete.setImageResource(R.drawable.ic_delete_disabled)
+        binding.btnRecord.setImageResource(R.drawable.ic_record)
+        binding.tvTimer.setText("00:00.00")
+        amplitudes = binding.waveformView.clear()
     }
 
     override fun onTimerTick(duration: String) {
         binding.tvTimer.text = duration // Use binding
+        binding.waveformView.addAmplitude(recorder.maxAmplitude.toFloat())
     }
 }
--- app/src/main/java/com/duymanh/audiorecorder/WaveformView.kt
+++ app/src/main/java/com/duymanh/audiorecorder/WaveformView.kt
@@ -11,15 +11,56 @@ import android.view.View
 class WaveformView(context: Context?, attrs: AttributeSet?): View(context, attrs) {
 
     private var paint = Paint()
+    private var amplitudes = ArrayList<Float>()
+    private var spikes = ArrayList<RectF>()
+
+    private var radius = 6f //do bo goc cua thanh spikes
+    private var w = 9f //do rong cua thanh spikes
+    private var d = 6f // khoang cach giua cac thanh spikes
+
+    private var sw = 0f //chieu rong cua tat ca cac thanh spike
+    private var sh = 200f // do cao toi da cua thanh spike
+
+    private var maxSpikes = 0 //so luong cac thanh spike max
 
     init {
         paint.color = Color.rgb(244, 81, 30)
+        sw = resources.displayMetrics.widthPixels.toFloat() //chieu rong cua thiet bi
+        maxSpikes = (sw/(w+d)).toInt()
+    }
+
+
+    fun addAmplitude(amp: Float) {
+        var norm: Float = Math.min(amp.toInt()/14, 200).toFloat()
+        amplitudes.add(norm)
+
+        spikes.clear()
+        var amps = amplitudes.takeLast(maxSpikes)
+        for(i in amps.indices){
+            var left = sw - i*(w+d)
+            var top = sh/2 - amps[i]/2
+            var right = left + w
+            var bottom = top + amps[i]
+            spikes.add(RectF(left,top,right,bottom))
+        }
+
+        invalidate()
+    }
+
+    fun clear(): ArrayList<Float>{
+        var amps = amplitudes.clone() as ArrayList<Float>
+        amplitudes.clear()
+        spikes.clear()
+        invalidate()
+
+        return amps
     }
 
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
-        canvas?.drawRoundRect(RectF(20f, 30f, 20f+30f, 30f + 60f), 0f, 0f, paint)
-        canvas?.drawRoundRect(RectF(60f, 60f, 60f+30f, 60f + 60f), 6f, 6f, paint)
+        spikes.forEach({
+            canvas?.drawRoundRect(it,radius,radius,paint)
+        })
     }
 
 }
