f6263cd1b94b16927a7df73098f4ea2ae63418e1
DuyManh1905 <93384645+DuyManh1905@users.noreply.github.com>
2024-10-16 17:10:46 +0700

    fix UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+    implementation ("com.google.android.material:material:latest_version")
+
+
     implementation("androidx.room:room-runtime:2.5.2")
     kapt("androidx.room:room-compiler:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
